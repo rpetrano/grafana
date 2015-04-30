b6224cd87e9aabf1cf7653e425b3d457c7f8ea11
Dieter Plaetinck <dieter@raintank.io>
2015-04-30 17:37:12 -0400

    WIP: initial alerting dispatcher and executor
    Conflicts:
    	main.go

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"strconv"
 	"time"
 
+	"github.com/grafana/grafana/pkg/alerting"
 	"github.com/grafana/grafana/pkg/cmd"
 	"github.com/grafana/grafana/pkg/log"
 	"github.com/grafana/grafana/pkg/metrics"
@@ -58,6 +59,8 @@ func main() {
 	if setting.ReportingEnabled {
 		go metrics.StartUsageReportLoop()
 	}
+	go alerting.Dispatcher()
+	go alerting.Executor()
 
 	cmd.StartServer()
 
